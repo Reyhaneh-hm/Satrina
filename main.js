366b83debce57b31932b00aecc03e91b70de1d88
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-08-30 22:24:02 +0330

    Make a slider

--- main.js
+++ main.js
@@ -3,4 +3,3 @@ import "./src/scss/base/index.scss";
 
 // Pablic Style
 import "./src/scss/components/public/index.scss";
-
--- src/js/pages/home/slider.js
+++ src/js/pages/home/slider.js
@@ -1,1 +1,23 @@
 import '../../../scss/components/pages/home/slider.scss';
+
+/*---------------------------------------*/
+
+
+import $ from "jquery";
+import "slick-carousel/slick/slick.min.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+$(document).ready(function () {
+    $('.sliderBox').slick({
+        rtl: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // dots: true,
+        arrows: true,
+        // prevArrow: $("#prevArrowHome"),
+        // nextArrow: $("#nextArrowHome"),
+    });
+});
