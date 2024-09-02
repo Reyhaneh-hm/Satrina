881cc5cb205ec6c91e6fad0c9ab409cb3cca5bff
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-09-02 17:53:18 +0330

    Add responsiv page showBlog

--- src/js/pages/showBlog/showBlog.js
+++ src/js/pages/showBlog/showBlog.js
@@ -1,1 +1,19 @@
 import '../../../scss/components/pages/showBlog/showBlog.scss';
+/*------------------------------------------------*/
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const filterBtn = document.querySelector("#filterBtn");
+    const modalFilter = document.querySelector(".modalFilter");
+    const overlay = document.querySelector(".overlay");
+    filterBtn.addEventListener("click", () => {
+        [overlay, modalFilter].forEach( (item) => item.classList.toggle("active"));
+    }
+    );
+    overlay.addEventListener("click", () => {
+        [overlay, modalFilter].forEach( (item) => item.classList.remove("active"));
+    }
+    );
+
+}
+)
