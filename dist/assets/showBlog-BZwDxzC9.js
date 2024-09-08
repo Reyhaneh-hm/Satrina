0a255e21468dfd5d0dac7ae6675f12b8813837cd
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-09-08 22:17:53 +0330

    Add dist

---
+++ dist/assets/showBlog-BZwDxzC9.js
@@ -0,0 +1,2 @@
+import"./main-BEYGiV2d.js";window.addEventListener("DOMContentLoaded",()=>{const c=document.querySelector("#filterBtn"),o=document.querySelector(".modalFilter"),e=document.querySelector(".overlay");c.addEventListener("click",()=>{[e,o].forEach(t=>t.classList.toggle("active"))}),e.addEventListener("click",()=>{[e,o].forEach(t=>t.classList.remove("active"))})});
+//# sourceMappingURL=showBlog-BZwDxzC9.js.map
