3c2d01786aa1620fe80aefebcd0be5a8e3e9e38b
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-12-12 12:06:24 +0330

    Add options

--- src/js/pages/home/hero.js
+++ src/js/pages/home/hero.js
@@ -20,3 +20,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+/*-----------------Saye-------------------*/
+
+document.querySelectorAll(".win-btn").forEach((b) => {
+  b.onmouseleave = (e) => {
+    // e.target.style.background = "black";
+    e.currentTarget.style.borderImage = null;
+  };
+  b.addEventListener("mousemove", (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+    const y = e.clientY - rect.top; //y position within the element.  
+    e.currentTarget.style.background = `radial-gradient(circle at ${x}px ${y}px , rgba(255,255,255,0.2),rgba(255,255,255,0) )`;
+  });
+});
