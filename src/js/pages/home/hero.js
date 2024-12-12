870401f1a9e2f0c70b0ce34c563e4b4a78411406
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-12-12 12:02:56 +0330

    Bug fix

--- src/js/pages/home/hero.js
+++ src/js/pages/home/hero.js
@@ -1,38 +1,22 @@
 import '../../../scss/components/pages/home/hero.scss';
 
-/*----------------Play Video---------------*/
+// /*----------------Play Video---------------*/
 document.addEventListener('DOMContentLoaded', function () {
 
-  var modal = document.getElementById("video-modal");
-  var playIcon = document.getElementById("play-icon");
-  var closeBtn = document.getElementsByClassName("close")[0];
+  const modal = document.getElementById("video-modal");
+  const playIcon = document.getElementById("play-icon");
+  const closeBtn = modal.querySelector(".close");
+  const video = document.getElementById("modalVideo");
 
   playIcon.addEventListener('click', function () {
-    modal.style.display = "block";
+    modal.style.display = "flex";
   });
 
   closeBtn.addEventListener('click', function () {
     modal.style.display = "none";
-  });
-
-  window.addEventListener('click', function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
     }
-  });
-});
-
-/*----------------------------------------*/
-
-document.querySelectorAll(".win-btn").forEach((b) => {
-  b.onmouseleave = (e) => {
-    // e.target.style.background = "black";
-    e.currentTarget.style.borderImage = null;
-  };
-  b.addEventListener("mousemove", (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left; //x position within the element.
-    const y = e.clientY - rect.top; //y position within the element.  
-    e.currentTarget.style.background = `radial-gradient(circle at ${x}px ${y}px , rgba(255,255,255,0.2),rgba(255,255,255,0) )`;
   });
 });
