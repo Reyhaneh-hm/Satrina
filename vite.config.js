2a4edcea987c2bb35e5634f5d9446a09aa28629b
Reyhaneh-hm <reyhaneh3784@gmail.com>
2025-01-16 11:00:05 +0330

    Add random code

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,16 @@ export default {
         showBlog: resolve(__dirname, "./showBlog.html"),
         showPost: resolve(__dirname, "./showPost.html"),
       },
+      output: {
+        assetFileNames: (assetInfo) => {
+          // تنظیم خروجی فایل برای تصاویر به عنوان فایل جداگانه
+          if (/\.(png|jpe?g|gif|svg)$/.test(assetInfo.name)) {
+            return 'images/[name][extname]';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
     },
+    assetsInlineLimit: 0,
   },
 };
